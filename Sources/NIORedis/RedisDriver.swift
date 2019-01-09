65d0afdfd02b3ef9ba702ae0fb896598655f1749
Nathan Harris <nathan.harris@globant.com>
2019-01-09 15:45:55 -0800

    Add `RedisDriver` to eventually replace `NIORedis` that works more deeply with NIO

--- Sources/NIORedis/RedisDriver.swift
+++ Sources/NIORedis/RedisDriver.swift
@@ -1,9 +1,7 @@
 import NIO
 import NIOConcurrencyHelpers
 
-/// A factory that handles all necessary details for creating connections to a Redis database instance.
-@available(*, deprecated)
-public final class NIORedis {
+public final class RedisDriver {
     /// The threading model to use for asynchronous tasks.
     ///
     /// Using `.eventLoopGroup` will allow an external provider to handle the lifetime of the `EventLoopGroup`,
@@ -14,61 +12,64 @@ public final class NIORedis {
     }
 
     private let executionModel: ExecutionModel
-    private let elg: EventLoopGroup
+    private let eventLoopGroup: EventLoopGroup
+
     private let isRunning = Atomic<Bool>(value: true)
 
     deinit { assert(!isRunning.load(), "Redis driver was not properly shut down!") }
 
-    /// Creates a handle to create connections to a Redis instance using the `ExecutionModel` provided.
-    /// - Parameter executionModel: The model to use for handling asynchronous scheduling.
+    /// Creates a driver instance to create connections to a Redis.
+    /// - Important: Call `terminate()` before deinitializing to properly cleanup resources.
+    /// - Parameter executionModel: The model to use for handling connection resources.
     public init(executionModel model: ExecutionModel) {
         self.executionModel = model
 
         switch model {
         case .spawnThreads(let count):
-            self.elg = MultiThreadedEventLoopGroup(numberOfThreads: count)
+            self.eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: count)
         case .eventLoopGroup(let group):
-            self.elg = group
+            self.eventLoopGroup = group
         }
     }
 
-    /// Creates a new `NIORedisConnection` with the connection parameters provided.
+    /// Handles the proper shutdown of managed resources.
+    /// - Important: This method should always be called, even when running in `.eventLoopGroup` mode.
+    public func terminate() throws {
+        guard isRunning.exchange(with: false) else { return }
+
+        switch executionModel {
+        case .spawnThreads: try self.eventLoopGroup.syncShutdownGracefully()
+        case .eventLoopGroup: return
+        }
+    }
+
+    /// Creates a new `RedisConnection` with the parameters provided.
     public func makeConnection(
         hostname: String = "localhost",
         port: Int = 6379,
         password: String? = nil
-    ) -> EventLoopFuture<NIORedisConnection> {
-        let channelHandler = RedisMessenger(on: elg.next())
-        let bootstrap = ClientBootstrap(group: self.elg)
+    ) -> EventLoopFuture<RedisConnection> {
+        let bootstrap = ClientBootstrap(group: eventLoopGroup)
             .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
             .channelInitializer { channel in
                 return channel.pipeline.addHandlers(
                     RESPEncoder(),
                     ByteToMessageHandler(RESPDecoder()),
-                    channelHandler
+                    RedisCommandHandler()
                 )
             }
 
         return bootstrap.connect(host: hostname, port: port)
-            .map { return NIORedisConnection(channel: $0, handler: channelHandler) }
+            .map { return RedisConnection(channel: $0) }
             .then { connection in
                 guard let pw = password else {
-                    return self.elg.next().makeSucceededFuture(result: connection)
+                    return self.eventLoopGroup.next().makeSucceededFuture(result: connection)
                 }
 
-                return connection.authorize(with: pw).map { _ in return connection }
+                #warning("TODO - Fix command extensions")
+                return self.eventLoopGroup.next().makeSucceededFuture(result: connection)
+                // return connection.authorize(with: pw).map { _ in return connection }
             }
-    }
-
-    /// Handles the proper shutdown of managed resources.
-    /// - Important: This method should always be called before deinit.
-    public func terminate() throws {
-        guard isRunning.exchange(with: false) else { return }
-
-        switch executionModel {
-        case .spawnThreads: try self.elg.syncShutdownGracefully()
-        case .eventLoopGroup: return
-        }
     }
 }
 
