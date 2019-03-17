5c32b97a0c5cd3e560dd2a7d465b51686915b62a
Nathan Harris <nathan@mordil.info>
2019-03-17 14:06:21 -0700

    Remove DispatchRedis

--- Package.swift
+++ Package.swift
@@ -5,15 +5,13 @@ import PackageDescription
 let package = Package(
     name: "nio-redis",
     products: [
-        .library(name: "NIORedis", targets: ["NIORedis"]),
-        .library(name: "DispatchRedis", targets: ["DispatchRedis"])
+        .library(name: "NIORedis", targets: ["NIORedis"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", .branch("master"))
     ],
     targets: [
         .target(name: "NIORedis", dependencies: ["NIO"]),
-        .target(name: "DispatchRedis", dependencies: ["NIORedis"]),
         .testTarget(name: "NIORedisTests", dependencies: ["NIORedis", "NIO"])
     ]
 )
