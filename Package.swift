ed106a943f97b889d2784cbd606cc30d2b7d5e71
Nathan Harris <nathan@mordil.info>
2019-04-10 11:33:52 -0700

    Update to use official release of SwiftLog

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "NIORedis", targets: ["NIORedis"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
