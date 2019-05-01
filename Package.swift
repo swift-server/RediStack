c7a606d8a650b9d5f7a96b3b2f77e735b7da30a4
Nathan Harris <nathan@mordil.info>
2019-05-01 16:24:05 -0700

    Add Copyright Notices, Acknowledgements, and Contributors List

--- Package.swift
+++ Package.swift
@@ -1,4 +1,17 @@
 // swift-tools-version:5.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the NIORedis open source project
+//
+// Copyright (c) 2019 NIORedis project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of NIORedis project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
--- Sources/NIORedis/ChannelHandlers/RedisCommandHandler.swift
+++ Sources/NIORedis/ChannelHandlers/RedisCommandHandler.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the NIORedis open source project
+//
+// Copyright (c) 2019 NIORedis project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of NIORedis project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import struct Foundation.UUID
 import Logging
 import NIO
