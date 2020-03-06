973a9162affd79286b7637f17dcfca808b282c4f
Nathan Harris <nathan@mordil.info>
2020-03-06 03:13:19 +0000

    70 -- Update SwiftMetrics for ecosystem compatability

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-metrics.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
