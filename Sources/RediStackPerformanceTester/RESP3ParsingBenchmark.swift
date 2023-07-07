f7bed03bcb83f7b2c97101af5f84cf8bae4569cb
Joannis Orlandos <joannis@orlandos.nl>
2023-07-07 10:28:26 +0200

    Add license headers

--- Sources/RediStackPerformanceTester/RESP3ParsingBenchmark.swift
+++ Sources/RediStackPerformanceTester/RESP3ParsingBenchmark.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the RediStack open source project
+//
+// Copyright (c) 2023 RediStack project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of RediStack project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import NIOCore
 import RESP3
 
@@ -7,7 +21,7 @@ func benchmarkRESP3Parsing() throws {
         .blobString(ByteBuffer(string: "GET")),
         .blobString(ByteBuffer(string: "welcome")),
     ]
-    
+
     try benchmark {
         var valueBuffer = valueBuffer
 
