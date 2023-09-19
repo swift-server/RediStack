1b69101da17f298891ac7506c9b54a2d334775c5
Fabian Fett <fabianfett@apple.com>
2023-09-19 15:29:38 +0200

    Add `RedisClusterNodeDescription` and `RedisClusterNodeID` (#92)

---
+++ Sources/RediStack/Cluster/RedisClusterNodeID.swift
@@ -0,0 +1,26 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the RediStack open source project
+//
+// Copyright (c) 2023 RediStack project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of RediStack project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// Properties to identifiy a Node in a Redis cluster
+public struct RedisClusterNodeID: Hashable, Sendable {
+    /// The node's endpoint. This might be the hostname (preferred) or ip address
+    public var endpoint: String
+    /// The node's redis port
+    public var port: Int
+
+    public init(endpoint: String, port: Int) {
+        self.endpoint = endpoint
+        self.port = port
+    }
+}
